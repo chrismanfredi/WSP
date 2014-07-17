c2a3bd4d83b251aace4d2d7a42c860cf0e450b3e
cmbaseball94 <cmbaseball94@gmail.com>
2014-07-17 18:18:48 -0400

    updates
    updates

--- Week 1/Manfredi_Christopher_Project/js/main.js
+++ Week 1/Manfredi_Christopher_Project/js/main.js
@@ -1,4 +1,16 @@
+(function($){	
+
 //Open the menu
     jQuery("#hamburger").click(function() {
         jQuery("#hamburgermenu").toggleClass("show")
     });
+
+
+$( ".mydatepicker" ).datepicker();
+	
+})(jQuery) // end private scope
+
+
+
+
+
